533ce4e895e952f0a05cd8472599a1e87fcea8d9
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-04-02 00:39:47 +0100

    Updates front page

--- wp-content/themes/fct001_dev/theme/front-page.php
+++ wp-content/themes/fct001_dev/theme/front-page.php
@@ -1,10 +1,44 @@
 <?php get_header(); ?>
 
 <main class="site__main">
-    <?php
-    $user = get_userdata(2);
-    print_r(get_role('client'));
-    ?>
+    <?php if( have_posts() ) : ?>
+
+        <?php while( have_posts() ) : the_post(); ?>
+            <?php
+                $pageId = get_the_ID();
+                $title = get_field('title', $pageId);
+                $description = get_field('description', $pageId);
+                $ctas = get_field('ctas', $pageId);
+            ?>
+
+            <h1>
+                <?php echo $title; ?>
+            </h1>
+
+            <div>
+                <?php echo $description; ?>
+            </div>
+
+            <div class="cta">
+                <?php foreach ($ctas as $cta) : ?>
+                    <div class="cta__item">
+                        <?php SDP_Link::the_link($cta, 'button'); ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
+        <?php endwhile; ?>
+
+
+
+    <?php else : ?>
+
+        <h1 class="offer__no-posts">
+            <?php _e('No special Offers yet, more to come soon..!', 'gmv001'); ?>
+        </h1>
+
+    <?php endif; ?>
+
 </main>
 
 <?php get_footer(); ?>
