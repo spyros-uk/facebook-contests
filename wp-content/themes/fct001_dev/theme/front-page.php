3d591e0fd955c93e1cf5b5642cfe3e94dce0f018
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-04-02 20:57:06 +0100

    reformats front-page

--- wp-content/themes/fct001_dev/theme/front-page.php
+++ wp-content/themes/fct001_dev/theme/front-page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
 <main class="site__main">
-    <?php if( have_posts() ) : ?>
+    <?php if (have_posts()) : ?>
 
-        <?php while( have_posts() ) : the_post(); ?>
+        <?php while (have_posts()) : the_post(); ?>
             <?php
-                $pageId = get_the_ID();
-                $title = get_field('title', $pageId);
-                $description = get_field('description', $pageId);
-                $ctas = get_field('ctas', $pageId);
+            $pageId = get_the_ID();
+            $title = get_field('title', $pageId);
+            $description = get_field('description', $pageId);
+            $ctas = get_field('ctas', $pageId);
             ?>
 
             <h1>
@@ -29,14 +29,10 @@
 
         <?php endwhile; ?>
 
-
-
     <?php else : ?>
-
         <h1 class="offer__no-posts">
             <?php _e('No special Offers yet, more to come soon..!', 'gmv001'); ?>
         </h1>
-
     <?php endif; ?>
 
 </main>
