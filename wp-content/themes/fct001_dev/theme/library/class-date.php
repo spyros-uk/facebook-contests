3e9f801a54de63ac0f6e79065fe59e41664bb44e
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-04-03 22:39:35 +0100

    Adds date helper class

---
+++ wp-content/themes/fct001_dev/theme/library/class-date.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * DATE
+ * Helpers to manipulate date objects
+ */
+
+
+/**
+ * Class SDP_BEM
+ *
+ */
+class SDP_DATE {
+
+    // http://php.net/manual/en/function.date-default-timezone-set.php
+    // http://php.net/manual/en/function.getdate.php | timestamp unix
+    // date_diff
+    //
+
+    public function get_date_diff($draw) {
+
+    }
+
+    public function the_draw_status() {
+
+    }
+}
