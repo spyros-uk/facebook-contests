baf0f126a7bc5aad58638ba5cc03052e55f7c9cd
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-04-03 22:39:21 +0100

    Adds draws helper class

---
+++ wp-content/themes/fct001_dev/theme/includes/class-draws.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DRAWS
+ *
+ */
+
+
+/**
+ * Class SDP_BEM
+ *
+ */
+class SDP_DRAWS {
+
+    public function get_draw_status($draw) {
+
+    }
+
+    public function the_draw_status() {
+
+    }
+}
