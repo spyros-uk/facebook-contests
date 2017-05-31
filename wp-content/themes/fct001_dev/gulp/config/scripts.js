0bf916951435a88c535a0efeee63685ae617f5c8
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-05-31 22:11:19 +0100

    Adds JQuery :'(

--- wp-content/themes/fct001_dev/gulp/config/scripts.js
+++ wp-content/themes/fct001_dev/gulp/config/scripts.js
@@ -4,13 +4,23 @@
  * Config
  *
  */
+
+var webpack = require('webpack');
+
 module.exports = {
     options: {
         webpack: {
             defaults: {
                 module: {
                     preLoaders: false
-                }
+                },
+                plugins: [
+                    new webpack.ProvidePlugin({
+                        '$': 'jquery',
+                        'jQuery': 'jquery',
+                        'window.jQuery': 'jquery'
+                    })
+                ]
             }
         }
     }
