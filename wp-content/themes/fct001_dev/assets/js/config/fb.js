a5f7c1392537111ca1d140c1abf6cdbced61ca5e
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-07-16 19:31:17 +0100

    Adds FB JS SDK

--- wp-content/themes/fct001_dev/assets/js/config/fb.js
+++ wp-content/themes/fct001_dev/assets/js/config/fb.js
@@ -8,3 +8,68 @@ if (window.FB) {
         redirect_uri: 'YOUR_URL'
     }, function(response){});
 }
+
+
+window.FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+
+/*
+
+
+ {
+ status: 'connected',
+ authResponse: {
+ accessToken: '...',
+ expiresIn:'...',
+ signedRequest:'...',
+ userID:'...'
+ }
+ }
+
+
+ <fb:login-button
+ scope="public_profile,email"
+ onlogin="checkLoginState();">
+ </fb:login-button>
+
+
+
+ function checkLoginState() {
+ FB.getLoginStatus(function(response) {
+ statusChangeCallback(response);
+ });
+ }
+
+
+ FB.api('/me', function(response) {
+ console.log(JSON.stringify(response));
+ });
+
+
+
+*/
+/*
+
+<script>
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '775901495916688',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+*/
