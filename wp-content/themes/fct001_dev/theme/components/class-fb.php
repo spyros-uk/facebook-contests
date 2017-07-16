df0d32b0bd0661e87f8f9739afba478524ccde7f
Spyros Papaioannou <SDPapaioannou@gmail.com>
2017-07-16 19:30:52 +0100

    Reformats class FB

--- wp-content/themes/fct001_dev/theme/components/class-fb.php
+++ wp-content/themes/fct001_dev/theme/components/class-fb.php
@@ -9,14 +9,25 @@ class Component_Fb {
     public static function include_fb_sdk() {
         ob_start(); ?>
 
-        <div id="fb-root"></div>
-        <script>(function(d, s, id) {
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId      : '615347261977517',
+                    cookie     : true,
+                    xfbml      : true,
+                    version    : 'v2.8'
+                });
+                FB.AppEvents.logPageView();
+            };
+
+            (function(d, s, id){
                 var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
+                if (d.getElementById(id)) {return;}
                 js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=615347261977517";
+                js.src = "//connect.facebook.net/en_US/sdk.js";
                 fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
 
      <?php return ob_get_clean();
     }
