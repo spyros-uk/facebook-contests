31586381fc526c483d0371ea461a309aa77fb106
Spyros Papaioannou <spyros.papaioannou@camelotglobal.com>
2017-10-01 15:25:11 +0100

    Adds default fb-metadata template to fb-class

--- wp-content/themes/fct001_dev/theme/components/class-fb.php
+++ wp-content/themes/fct001_dev/theme/components/class-fb.php
@@ -31,4 +31,16 @@ class Component_Fb {
 
      <?php return ob_get_clean();
     }
+
+    public static function insert_fb_meta_data() {
+        ob_start(); ?>
+
+        <meta property="og:url"           content="http://www.your-domain.com/your-page.html" />
+        <meta property="og:type"          content="website" />
+        <meta property="og:title"         content="Your Website Title" />
+        <meta property="og:description"   content="Your description" />
+        <meta property="og:image"         content="http://www.your-domain.com/path/image.jpg" />
+
+        <?php return ob_get_clean();
+    }
 }
