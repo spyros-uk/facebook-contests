823fd78cae206b3530a2a47f5f01c0efaf99ad3f
Spyros Papaioannou <spyros.papaioannou@camelotglobal.com>
2017-10-31 00:40:14 +0000

    Adds mail class
    - handles subject
    - handles message
    - sends email

---
+++ wp-content/themes/fct001_dev/theme/includes/class-mail.php
@@ -0,0 +1,22 @@
+<?php
+class SDP_MAIL {
+
+    public static function get_subject() {
+        return 'You are a winner!';
+    }
+
+    public static function get_populated_email_template($name, $contest) {
+        return "Dear $name,
+        Congratulations! You are the winner of the contest '$contest->post_title'.
+        We will soon contact you again, to let you know how to claim your prize!
+        
+        Sincerely,
+        Facebook contests team
+        ";
+    }
+
+    public static function send_email($to, $subject, $message) {
+        wp_mail($to, $subject, $message);
+    }
+}
+?>
