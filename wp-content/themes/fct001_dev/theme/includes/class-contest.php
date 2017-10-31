d5b58666f2d9ffc1a6e2dccb87b395934881e5c2
Spyros Papaioannou <spyros.papaioannou@camelotglobal.com>
2017-10-31 00:41:05 +0000

    Updates contest class
    - Adds winner notification functionality

--- wp-content/themes/fct001_dev/theme/includes/class-contest.php
+++ wp-content/themes/fct001_dev/theme/includes/class-contest.php
@@ -2,13 +2,13 @@
 
 class SDP_CONTEST {
 
-    public static function get_nominees($contest_id) {
-        $permalink = get_the_permalink($contest_id);
+    static function get_nominees($contest) {
+        $permalink = get_the_permalink($contest->ID);
         $contest_likes = SDP_FB::get_object_likes($permalink);
         return $contest_likes;
     }
 
-    public static function pick_winner($nominees_list) {
+    static function pick_winner($nominees_list) {
         $rand_keys = array_rand($nominees_list, 1);
         $winner_fb_id = $nominees_list[$rand_keys]['id'];
         $winner = SDP_FB::get_user_data($winner_fb_id);
@@ -16,12 +16,23 @@ class SDP_CONTEST {
         return $winner;
     }
 
-    public static function get_winner($contest_id) {
-        return get_field('winner', $contest_id)[0];
+    static function notify_winner($winner_data, $contest) {
+        $subject = SDP_MAIL::get_subject();
+        $message = SDP_MAIL::get_populated_email_template($winner_data['winner_name'], $contest);
+
+        SDP_MAIL::send_email(
+            $winner_data['winner_email'],
+            $subject,
+            $message
+        );
     }
 
-    public static function is_winner_set($contest_id) {
-        $winner_data = self::get_winner($contest_id);
+    public static function get_winner($contest) {
+        return get_field('winner', $contest->ID)[0];
+    }
+
+    public static function is_winner_set($contest) {
+        $winner_data = self::get_winner($contest);
         $is_winner = null;
         foreach ($winner_data as $data) {
             $is_winner = (
@@ -32,23 +43,20 @@ class SDP_CONTEST {
         return $is_winner;
     }
 
-    public static function set_winner($contest_id) {
-        $nominees_list = self::get_nominees($contest_id);
+    public static function set_winner($contest) {
+        $nominees_list = self::get_nominees($contest->ID);
         $winner = self::pick_winner($nominees_list);
 
         $winner_data = array(
             array(
-                "winner_fb_id"	=> $winner['id'],
-                "winner_name"	=> $winner['name'],
-                "winner_email"	=> $winner['email']
+                'winner_fb_id'	=> $winner['id'],
+                'winner_name'	=> $winner['name'],
+                'winner_email'	=> $winner['email']
             )
         );
 
-        update_field('winner', $winner_data, $contest_id);
-    }
-
-    public static function notify_winner() {
-
+        update_field('winner', $winner_data, $contest->ID);
+        self::notify_winner($winner_data, $contest);
     }
 }
 
